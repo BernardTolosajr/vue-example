f7a4b2276d60ad31fbd553c7f478eb42de1d102a
Bernardo Tolosa <bernardo.tolosa@GFIMAC0001.local>
2018-04-11 11:01:18 +0800

    initial commit

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <button v-on:click="doCountPlus">You clicked me {{ count }} times.</button>
+</template>
+
+<script type = "text/javascript" >
+export default {
+  name: 'button-counter',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    doCountPlus () {
+       this.count++;
+    }
+  }
+};
---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button-counter />
+    <ul>
+      <li v-for="todo in todos">
+        {{ todo.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+import ButtonCounter from './ButtonCounter'
+
+export default {
+  name: 'TodoList',
+  components: {
+    ButtonCounter
+  },
+  data() {
+    return {
+      todos: [
+        {name: 'a'},
+        {name: 'b'}
+      ]
+    }
+  }
+};
+
+</script>
+<style>
+</style>
